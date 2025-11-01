a91477c5c3e13e766f0e8415852b39cb913c7e0e
jldec <jurgen@jldec.me>
2025-11-01 19:20:03 +0000

    polish, 400 instead of 404

--- index.ts
+++ index.ts
@@ -5,6 +5,11 @@ type Colo = Record<string, string | number>
 
 /**
  * GET colo JSON  with timing from `https://${coloName}.jldec.me/getcolo`
+ * Possibilities for coloName include:
+ * - getcolo.jldec.me: closest worker to this server
+ * - geo.jldec.me: server resolved by shared tunnel endpoint
+ * - <city>.jldec.me: server in a specific city
+ *
  * @param coloName - subdomain of jldec.me to query
  */
 async function getColo(coloName: string): Promise<Response> {
@@ -14,7 +19,7 @@ async function getColo(coloName: string): Promise<Response> {
   try {
     resp = await fetch(url)
   } catch (e) {
-    throw new Error(`Error fetching ${url}: ${e}`)
+    throw new Error(`${url}: ${e}`)
   }
   if (!resp.ok) throw new Error(`Status ${resp.status} fetching ${url}`)
   let colo: Colo
@@ -31,21 +36,22 @@ async function getColo(coloName: string): Promise<Response> {
 Bun.serve({
   port: PORT,
   async fetch(req) {
+    console.log(req.method, req.url)
     const url = new URL(req.url)
     const pathname = url.pathname
     const coloName = pathname.slice(1)
-    console.log(req.method, req.url)
     try {
+      if (req.method !== 'GET') return new Response('Method Not Allowed', { status: 405 })
       if (pathname === '/') return new Response(`Hello from ${url.origin}`)
-      // Check if valid DNS hostname (RFC 1123, no dots, 1-63 chars, alphanum or hyphen, not start/end with hyphen)
+      // Check if valid DNS hostname: RFC 1123, no dots, 1-63 chars, alphanum or hyphen, not start/end with hyphen
       // This also catches requests for favicon etc.
       if (!/^(?!-)[A-Za-z0-9-]{1,63}(?<!-)$/.test(coloName)) throw new Error(`Invalid colo hostname ${coloName}`)
       return await getColo(coloName)
-    } catch (e: any) {
-      console.error(`404 ${e.message}`)
-      return new Response('not found', { status: 404 })
+    } catch (e) {
+      console.log(`400 ${e}`)
+      return new Response(`${e}`, { status: 400 })
     }
   }
 })
 
-console.log(`Bun running on :${PORT}`)
+console.log(`Bun running on http://localhost:${PORT}`)
